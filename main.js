72c940e0c71b1ad3e83918b6696d916cae257daf
Aqsa Naseer <163171532+aqsanaseer11@users.noreply.github.com>
2024-04-01 20:47:42 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addtask = await inquirer.prompt([{
+            name: "todo",
+            message: "What do you want to add in your Todos?",
+            type: "input"
+        },
+        {
+            name: "addmore",
+            message: "Do you want to add more?",
+            type: "confirm",
+            default: false
+        }]);
+    todos.push(addtask.todo);
+    condition = addtask.addmore;
+    console.log(todos);
+}
+;
+let clearList = await inquirer.prompt([
+    {
+        name: "clear",
+        type: "confirm",
+        message: "Do you want to clear your ToDo list?",
+        default: false,
+    },
+]);
+if (clearList.clear) {
+    todos = [];
+    console.log("ToDo list cleared!");
+}
+else {
+    console.log("ToDo list not cleared!");
+}
+console.log("Final ToDo List:");
+console.log(todos);
+// console.log(todos)
